e0b65860f9d162b58a6a43cd454c6d882ef556e1
valentin-dmx <valentin.demouveaux@eleve.ensai.fr>
2026-02-27 23:00:23 +0100

    feat: segmentation des modules de dao faisant le lien avec l'api spoonacular, et implémentation (pour l'instant incomplète)des modules concernant plat et planning repas

--- backend/dao/spoonacular_dao/spoonacular_dao_plat.py
+++ backend/dao/spoonacular_dao/spoonacular_dao_plat.py
@@ -1,49 +1,14 @@
-import os
-
-import requests
-
+from backend.dao.spoonacular_dao.spoonacular_dao import SpoonacularDAO
 from backend.dto.ingredient_dto import IngredientDTO
 from backend.dto.plat_dto import PlatDTO
 
 
-class SpoonacularDAO:
+class SpoonacularDAOPlat(SpoonacularDAO):
     """
-    Data Access Object (DAO) pour interagir avec l'API Spoonacular.
-    Fournit des méthodes pour rechercher des plats et récupérer leurs informations.
-    """
+    Data Access Object (DAO) pour interagir avec l'API Spoonacular spécifiquement pour les plats.
+    Fournit des méthodes pour rechercher des plats, récupérer leurs informations et leurs ingrédients."""
 
-    BASE_URL = "https://api.spoonacular.com"
     NOMBRE_RESULTATS = 2
-
-    def __init__(self):
-        self.api_key = os.environ.get("SPOONACULAR_API_KEY")
-        if not self.api_key:
-            raise RuntimeError("Variable d'environnement SPOONACULAR_API_KEY manquante")
-
-    def _get(self, endpoint, params=None):
-        """
-        Effectue une requête GET à l'API Spoonacular.
-
-        Params
-        ------------
-            endpoint: str
-                Le point de terminaison de l'API à appeler.
-            params: dict, optional
-                Les paramètres de la requête (par défaut: None).
-        Return
-        ------------
-            dict
-                La réponse de l'API sous forme de dictionnaire.
-        """
-        if params is None:
-            params = {}
-
-        params["apiKey"] = self.api_key
-
-        response = requests.get(f"{self.BASE_URL}{endpoint}", params=params, timeout=10)
-
-        response.raise_for_status()
-        return response.json()
 
     def recherche_plat_nom(self, query, number=NOMBRE_RESULTATS):
         """
