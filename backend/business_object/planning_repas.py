119d79ec822d77f4b5e135f711f472ffc7f42041
Antoine-Blot <antoine.blot@eleve.ensai.fr>
2026-03-03 18:21:41 +0100

    Revert "feat: planning de repas (plats) (#16)"
    This reverts commit 9be532f2c0dbd52f7ed4b234a49ff68b1e303da6.

--- backend/business_object/planning_repas.py
+++ backend/business_object/planning_repas.py
@@ -1,25 +1,17 @@
-from backend.business_object.jour_repas import JourRepas
-
-
 class PlanningRepas:
     """
-    Classe représentant un planning de repas sur plusieurs jours.
-    Chaque jour est représenté par une instance de JourRepas.
+    Classe représentant un plan de repas.
+    Comprend une série de plats pour chaque moment de la journée (petit-déjeuner, déjeuner, dîner), sur une série de plusieurs jours.
     """
 
-    def __init__(self, id_utilisateur=None, id=None, nom=None, nb_jours=None):
-        self.id_utilisateur = id_utilisateur
-        self.id = id
-        self.nom = nom
+    def __init__(self, nb_jours):
         self.nb_jours = nb_jours
         for i in range(1, nb_jours + 1):
-            setattr(self, f"jour_{i}", JourRepas())
+            setattr(
+                self,
+                f"jour_{i}",
+                {"petit_dejeuner": None, "dejeuner": None, "diner": None},
+            )
 
     def __str__(self):
-        jours_str = ", ".join(
-            [
-                f"jour_{i}={getattr(self, f'jour_{i}')} "
-                for i in range(1, self.nb_jours + 1)
-            ]
-        )
-        return f"PlanningRepas(id_utilisateur={self.id_utilisateur}, id={self.id}, nom={self.nom}, nb_jours={self.nb_jours}, {jours_str})"
+        return f"PlanningRepas(nb_jours={self.nb_jours}, jours={[getattr(self, f'jour_{i}') for i in range(1, self.nb_jours + 1)]})"
--- backend/dto/plat_dto.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class PlatDTO(BaseModel):
-    id: int
-    nom: str
