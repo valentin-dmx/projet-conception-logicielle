ef36698d85f1bf0f12903888eebb8174c80ab8eb
Antoine-Blot <antoine.blot@eleve.ensai.fr>
2026-03-03 18:21:41 +0100

    Revert "feat: ajout du panier (liste de courses) (#15)"
    This reverts commit 41f0a328f6ecc211013f841098facbbf9b78ec00.

--- backend/dto/ingredient_dto.py
+++
@@ -1,8 +0,0 @@
-class IngredientDTO:
-    def __init__(
-        self, nom: str, quantite: float, unite: str, prix_estime: float | None = None
-    ):
-        self.nom = nom
-        self.quantite = quantite
-        self.unite = unite
-        self.prix_estime = prix_estime
